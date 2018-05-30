6a3445901d6171cdd1ba3d1a51d6c17ff075c05f
Kris Nye <krisnye@gmail.com>
2018-05-29 22:25:13 -0700

    deleting local

--- src/ESTree.ts
+++ src/ESTree.ts
@@ -2,7 +2,7 @@
 export class Position {
     line: number
     column: number
-    constructor(line:number,column:number) {
+    constructor(line:number, column: number) {
         this.line = line        // >= 1
         this.column = column    // >= 0
     }
